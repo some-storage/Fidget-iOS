df158c99b83b843259c392e2650f17cdeb87777b
Byung jin <bjcho1503@naver.com>
2024-01-08 15:29:46 +0900

    ⚙️ :: project setting

---
+++ Fidget/Fidget/Source/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Fidget
+//
+//  Created by 조병진 on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
